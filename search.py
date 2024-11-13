439d2d638e41c1bde200e0836a78f0cd157b314f
FilinMen <fiilinkadabra@tutamail.com>
2024-11-13 20:27:19 +0300

    the end

--- search.py
+++ search.py
@@ -1,36 +1,44 @@
 import random 
-#линейный поиск
+
+print("ПЕРВЫЙ СПИСОК")
 list_1 = random.sample(range(1000001), 100)#sample позволяет выбрать случайные элементы из указанного диапазона без дубликатов.
 print(list_1)
 
-element = int(input('искомый элемент:'))
+print('ВТОРОЙ СПИСОК')
+list_2 = random.sample(range(1000001), 100)#sample позволяет выбрать случайные элементы из указанного диапазона без дубликатов.
+list_2.sort()
+print(list_2)
+
+el = int(input('искомый элемент:'))
+
 score = 0 #начинаем подсчет с 0 количество сравнений
-if element in list_1: #если эле-т находится в list_1
+if el in list_1: #если эле-т находится в list_1
     for i in range(0, len(list_1)):
         score = score + 1 #прибавляем каждый цикл +1
-        if list_1[i] == element:
+        if list_1[i] == el:
             break 
-    print(f'индекс искомого эл-та:{i}')
+    print(f'индекс искомого эл-та из первого списка:{i}')
+    print(f'количество сравнений в линейном поиске:{score}')
 else:
-    print("элемент не найден")
-print(f'количество сравнений:{score}')
-'''
+    print("элемент не найден в первом списке")
+
 #бинарный поиск
-lys = [10,20,30,40,50]
-val = 20
-
-first = 0
-last = len(lys)-1
-index = -1
-while (first <= last) and (index == -1):
-    mid = (first+last)//2
-    if lys[mid] == val:
-        index = mid
-    else:
-        if val<lys[mid]:
-            last = mid -1
-        else:
-            first = mid +1
-
-print(index)
-'''
+if el in list_2: #проверка на наличие в списке
+    first = 0
+    last = len(list_2)-1
+    score2 = 0
+    index = 1
+    while (first <= last) and index == 1:
+        mid = (first+last)//2 # индекс элемента в середине списка
+        score2 = score2 + 1 #прибавляем каждый цикл +1
+        if list_2[mid] == el:
+            index = mid
+        else: 
+            if el<list_2[mid]:
+                last = mid -1
+            else:
+                first = mid +1
+    print(f'количество сравнений в бинарном поиске:{score2}')
+    print(f'индекс искомого эл-та из второго списка:{index}')
+else:
+    print("элемент не найден во втором списке")
--- sort.py
+++ sort.py
@@ -7,13 +7,12 @@ while stop_flag:
     stop_flag = False # меняем значение true, если программа не сделает изменене она завершиться 
     for i in range(len(list_1) - 1): # -1 потому что в дальнейшем мы сравниваем с i + 1 
         score1 += 1 #прибавляем каждый цикл +1
+        print(list_1) #промежуточный
         if list_1[i] < list_1[i + 1]: 
             a = list_1[i] 
             list_1[i] = list_1[i + 1] #меняем местами
             list_1[i + 1] = a #меняем местами
             stop_flag = True
-    print(list_1)       
-print(score1)
 #сортировка выбором
 list_2 = [ran.randint(0, 1000000) for u in range(100)]
 score = 0
@@ -21,11 +20,13 @@ for i in range(0 ,len(list_2)):
     min = i
     for j in range(i + 1,len(list_2)):
         score += 1
+        print(list_2) #промежуточный
         if list_2[j] > list_2[min]:
             min = j
     b = list_2[i]
     list_2[i] = list_2[min]
     list_2[min] = b
-    print(list_2) 
-print(f'в первом списке:{score1}')
-print(f'во втором списке:{score}')
+print(f"ПЕРВЫЙ СОРТИРОВАННЫЙ СПИСОК:{list_1}")
+print(f"ВТОРОЙ СОРТИРОВАННЫЙ СПИСОК:{list_2}") 
+print(f'количество сравнений в первом списке:{score1}')
+print(f'количество сравнений во втором списке:{score}')
