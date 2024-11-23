7651dceff92b86b02b4ee3901276abd9faa94317
FilinMen <fiilinkadabra@tutamail.com>
2024-11-23 05:19:34 +0300

    0.01

--- search.py
+++ search.py
@@ -2,12 +2,8 @@ import random
 
 print("ПЕРВЫЙ СПИСОК")
 list_1 = random.sample(range(1000001), 100)#sample позволяет выбрать случайные элементы из указанного диапазона без дубликатов.
+list_1.sort()
 print(list_1)
-
-print('ВТОРОЙ СПИСОК')
-list_2 = random.sample(range(1000001), 100)#sample позволяет выбрать случайные элементы из указанного диапазона без дубликатов.
-list_2.sort()
-print(list_2)
 
 el = int(input('искомый элемент:'))
 
@@ -17,28 +13,29 @@ if el in list_1: #если эле-т находится в list_1
         score = score + 1 #прибавляем каждый цикл +1
         if list_1[i] == el:
             break 
-    print(f'индекс искомого эл-та из первого списка:{i}')
+    print(f'индекс искомого в линейном поиске:{i}')
     print(f'количество сравнений в линейном поиске:{score}')
 else:
     print("элемент не найден в первом списке")
 
 #бинарный поиск
-if el in list_2: #проверка на наличие в списке
+print(list_1) #сортированный первый список
+if el in list_1: #проверка на наличие в списке
     first = 0
-    last = len(list_2)-1
+    last = len(list_1)-1
     score2 = 0
     index = 1
     while (first <= last) and index == 1:
         mid = (first+last)//2 # индекс элемента в середине списка
         score2 = score2 + 1 #прибавляем каждый цикл +1
-        if list_2[mid] == el:
+        if list_1[mid] == el:
             index = mid
         else: 
-            if el<list_2[mid]:
+            if el<list_1[mid]:
                 last = mid -1
             else:
                 first = mid +1
     print(f'количество сравнений в бинарном поиске:{score2}')
-    print(f'индекс искомого эл-та из второго списка:{index}')
+    print(f'индекс искомого эл-та в бинарном поиске:{index}')
 else:
     print("элемент не найден во втором списке")
--- sort.py
+++ sort.py
@@ -1,6 +1,11 @@
 import random as ran
+
+unsorted_list_1 = [ran.randint(0, 1000000) for t in range(100)] #случайные элементы из указанного диапазона
+print(unsorted_list_1)
+list_2 = unsorted_list_1.copy()
+list_1 = unsorted_list_1.copy()
+
 #сортировка пузырьком
-list_1 = [ran.randint(0, 1000000) for t in range(100)] #случайные элементы из указанного диапазона
 score1 = 0 #начинаем подсчет с 0 количество сравнений
 stop_flag = True # стоп флаг если он имеет значение true запускает цикл
 while stop_flag:
@@ -9,24 +14,25 @@ while stop_flag:
     for i in range(len(list_1) - 1): # -1 потому что в дальнейшем мы сравниваем с i + 1 
         score1 += 1 #прибавляем каждый цикл +1
         if list_1[i] < list_1[i + 1]: 
-            a = list_1[i] 
+            ap = list_1[i] 
             list_1[i] = list_1[i + 1] #меняем местами
-            list_1[i + 1] = a #меняем местами
+            list_1[i + 1] = ap #меняем местами
             stop_flag = True
+
 #сортировка выбором
-list_2 = [ran.randint(0, 1000000) for u in range(100)]
 score = 0
-for i in range(0 ,len(list_2)):
-    min = i
-    for j in range(i + 1,len(list_2)):
+for a in range(0 ,len(list_2)):
+    min = a
+    print(list_2) #промежуточный
+    for j in range(a + 1,len(list_2)):
         score += 1
-        print(list_2) #промежуточный
         if list_2[j] > list_2[min]:
             min = j
-    b = list_2[i]
-    list_2[i] = list_2[min]
+    b = list_2[a]
+    list_2[a] = list_2[min]
     list_2[min] = b
-print(f"ПЕРВЫЙ СОРТИРОВАННЫЙ СПИСОК:{list_1}")
-print(f"ВТОРОЙ СОРТИРОВАННЫЙ СПИСОК:{list_2}") 
-print(f'количество сравнений в первом списке:{score1}')
-print(f'количество сравнений во втором списке:{score}')
+print(f"НЕ СОРТИРОВАННЫЙ СПИСОК:{unsorted_list_1}")
+print(f"СОРТИРОВАННЫЙ СПИСОК ПУЗЫРЬКОМ:{list_1}")
+print(f"СОРТИРОВАННЫЙ СПИСОК ВЫБОРОМ:{list_2}")
+print(f'количество сравнений в сортировке пузырьком:{score1}')
+print(f'количество сравнений в сортировке выборкой:{score}')
